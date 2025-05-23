521a72b558a59dc47f1933df41f3472a322b163d
Yole <igbavagodswill4@gmail.com>
2025-05-23 16:11:11 +0100

    Finished the static version of the  header section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,60 @@
+import cartoonPic from "./assets/profile_pic.png";
+
 export default function App() {
-  return <section></section>;
+  return (
+    <section className="app-container">
+      <Header />
+    </section>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        src={cartoonPic}
+        alt="cartoon pic of myself"
+        className="cartoon-pic"
+      />
+
+      <div className="intro-text">
+        <p>Hello, I'm Godswill Igbava.</p>
+        <p>Frontend Developer</p>
+
+        <p>I create engaging and responsive web experiences.</p>
+      </div>
+
+      <div className="intro-btns">
+        <Button border="none" className="intro-btn view-btn">
+          View Projects
+        </Button>
+
+        <Button border="solid 1px #1e90ff" className="intro-btn contact-btn">
+          Contact Me
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+// reusable button component
+function Button({
+  children,
+  className = "",
+  background = "#080E1B",
+  border = "",
+}) {
+  const btnStyle = {
+    background,
+    borderRadius: "0.7rem",
+    padding: " 1rem 1.5rem ",
+    border,
+    color: "#FFFFFF",
+  };
+
+  return (
+    <button style={btnStyle} className={className}>
+      {children}
+    </button>
+  );
 }
